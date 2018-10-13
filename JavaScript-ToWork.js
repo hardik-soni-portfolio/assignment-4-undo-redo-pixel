f54195f36958e70e1d8a4b4068840adfedd9619a
kihars <soni.har@husky.neu.edu>
2018-10-13 11:54:50 -0400

    added the remove item functionality along with some changes in the HTML CSS. Also, added icon to the HTML page and add button

--- JavaScript-ToWork.js
+++ JavaScript-ToWork.js
@@ -1,61 +1,90 @@
-var stuff = [];
-var undoStack = [];
-var redoStack = [];
+//Author HS:3 arrays to create maintain the undo-redo fucntionality
+var stuff = []; //Author HS: array to store values entered in the textbox
+var undoStack = []; //Author HS: array (of arrays) to be used while undo event occurs,
+var redoStack = []; //Author HS: array (of arrays) to be used while redo event occurs
 
 function buildItemList() {
+    //Author HS: on click of the "add" button a check for the previous UL element is made and deleted if already existing
+    //Author HS: after deletion, a new UL is created and each new line item (ToWork item) is created as LI element
+    //Author HS: and added to the newly created UL element
     var prevUl = document.getElementById("toWorkItems");
-    if (prevUl!==null) {
+    if (prevUl !== null) {
         prevUl.remove();
     }
-    var division = document.getElementById("containerToWork");
-    var ulCont = document.createElement("ul");
+    //Author HS: variable for main div container
+    var divCont = document.getElementById("containerToWork");
+    var ulCont = document.createElement("ul"); //Author HS: create a new UL element
     ulCont.id = "toWorkItems";
+
+    //Author HS: loop to create the HTML page depending on the number of items added in the stuff array
     for (var i = 0; i < stuff.length; i++) {
+        //Author HS: create new LI element for each new ToWork item
         var toWorkItem = document.createElement("li");
         toWorkItem.id = i;
 
+        //Author HS: create a checkbox with the event listener to check for a check/uncheck change after it is created
         var toWorkCheck = document.createElement("input");
-        toWorkCheck.className="checkbox"
+        toWorkCheck.className = "checkbox"
         toWorkCheck.type = "checkbox";
         toWorkCheck.id = i;
         toWorkCheck.addEventListener('change', function (e) {
+            //Author HS: identify the current checkbox that is checked/unchecked
             e = e || window.event;
             var targetCheck = e.targetCheck || e.srcElement
             if (targetCheck.checked) {
-                strikePara(parseInt(targetCheck.id));
+                strikePara(parseInt(targetCheck.id)); //Author HS: call the method to strike the P element for each LI element
             }
         }, false);
-
+        //Author HS: create a P element to use as a holder to display value entered in the textbox
         var paraItem = document.createElement("p");
         paraItem.id = i;
         paraItem.innerText = stuff[i].content;
+        //Author HS: check for isComplete status and design it accordingly,
+        //Author HS: meaning strikethrough if the isComplete is true else do nothing
+        if (stuff[i].isComplete === true) {
+            toWorkCheck.checked = stuff[i].isComplete;
+            paraItem.style = "text-decoration: line-through";
+        } else {
+            toWorkCheck.checked = stuff[i].isComplete;
+            toWorkCheck.style.removeProperty("text-decoration");
+        }
 
+        //Author HS: create a remove button for each input element
         var toWorkRemove = document.createElement("input");
         toWorkRemove.type = "button";
         toWorkRemove.id = i;
         toWorkRemove.value = "Remove Item";
         toWorkRemove.addEventListener('click', function (e) {
+            //Author HS: check for the remove button that is called
             e = e || window.event;
             var target = e.target || e.srcElement
             removeItemFromList(parseInt(target.id))
         }, false);
-
+        //Author HS: append child to LI elements
         toWorkItem.appendChild(toWorkCheck);
         toWorkItem.appendChild(paraItem);
         toWorkItem.appendChild(toWorkRemove);
-
+        //Author HS: append toWork LI element to main UL element
         ulCont.appendChild(toWorkItem);
     }
-    division.appendChild(ulCont);
+    //Author HS: append UL to the div container
+    divCont.appendChild(ulCont);
 }
-
+//Author HS: this method is invoked on every Click event of the "add ToWork Item" button
 function addItemToList() {
+    //Author HS: the content/value entered in the array is added to the stuff key/value array with a value of false for isComplete
     stuff.push({ content: document.getElementById("enterNewToWork").value, isComplete: false });
-    buildItemList();
+    redoStack = []; //Author HS: set the redoStack to null
+    undoStack.push(stuff.slice()); //Author HS: create a copy of stuff array and push it in the undoStack
+    buildItemList(); //Author HS: call this method to create elements on the HTML page
 }
 
 function removeItemFromList(i) {
-
+    //Author HS: remove the selected item form the stuff array
+    stuff.splice(i, 1);
+    redoStack = []; //Author HS: set the redoStack to null
+    undoStack.push(stuff.slice()); //Author HS: create a copy of stuff array and push it in the undoStack
+    buildItemList(); //Author HS: call this method to update the elements on the HTML page
 }
 function strikePara(i) {
 
